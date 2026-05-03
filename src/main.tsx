c02ef0afb99748097e6903c20403c8847c83e499
rishabh21g <biasedengineer@gmail.com>
2026-05-03 16:49:50 +0530

    feat: integrate new UI components and styles
    - Added Tailwind CSS imports and custom theme variables in index.css.
    - Updated main.tsx to include TooltipProvider and Toaster components.
    - Created components.json for component configuration.
    - Implemented Alert, Button, Card, and Tooltip components with respective variants and styles.
    - Developed Sonner toaster for notifications with custom icons and themes.
    - Added utility functions for class name management in utils.ts.

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,14 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import { Toaster } from 'sonner'
+import { TooltipProvider } from './components/ui/tooltip.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
+    <TooltipProvider>
     <App />
+    <Toaster/>
+    </TooltipProvider>
   </StrictMode>,
 )
